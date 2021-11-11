110d21771dec1d0b3d8e76db07de135e0326e2e8
Robert <18190294+roberto88480@users.noreply.github.com>
2021-11-11 12:48:14 +0100

    code cleanup and @SuppressWarnings("unused")

--- src/main/java/me/roberto88480/minecraftdiscordjdabot/minecraftevents/MinecraftPlayerQuitEvent.java
+++ src/main/java/me/roberto88480/minecraftdiscordjdabot/minecraftevents/MinecraftPlayerQuitEvent.java
@@ -1,7 +1,6 @@
 package me.roberto88480.minecraftdiscordjdabot.minecraftevents;
 
 import me.roberto88480.minecraftdiscordjdabot.DiscordMinecraftPlugin;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -13,8 +12,8 @@ public class MinecraftPlayerQuitEvent implements Listener {
     }
 
     @EventHandler
+    @SuppressWarnings("unused")
     public void onPlayerQuit(PlayerQuitEvent event) {
         discordMinecraftPlugin.updateActivity(true);
-        //System.out.println("Player QUIT");
     }
 }
