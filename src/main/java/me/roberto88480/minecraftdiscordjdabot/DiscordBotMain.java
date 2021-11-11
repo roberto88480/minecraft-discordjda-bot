aa0ddeb95d21bd4c8f8a8133fe6e77d3a69649ee
Robert <18190294+roberto88480@users.noreply.github.com>
2021-11-11 12:49:05 +0100

    remove unused import

--- src/main/java/me/roberto88480/minecraftdiscordjdabot/DiscordBotMain.java
+++ src/main/java/me/roberto88480/minecraftdiscordjdabot/DiscordBotMain.java
@@ -2,8 +2,6 @@ package me.roberto88480.minecraftdiscordjdabot;
 
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import javax.security.auth.login.LoginException;
-
 public class DiscordBotMain extends ListenerAdapter
 {
     public static void main(String[] args)
