9d5fbe1ecd9fcad36c19dd3b1e3f4353851777ac
Roberto88480 <18190294+roberto88480@users.noreply.github.com>
2022-08-28 14:15:45 +0200

    remove listenerAdapter from main

--- src/main/java/me/roberto88480/minecraft/discordjdabot/DiscordBotMain.java
+++ src/main/java/me/roberto88480/minecraft/discordjdabot/DiscordBotMain.java
@@ -1,8 +1,6 @@
 package me.roberto88480.minecraft.discordjdabot;
 
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-
-public class DiscordBotMain extends ListenerAdapter
+public class DiscordBotMain
 {
     public static void main(String[] args)
     {
